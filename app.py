65aed91117abffb5e240943d02945ba7ed87f42b
JCKolodziej <kuba@kolodziej.com.pl>
2019-08-17 19:30:06 +0200

    edit route

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.route('https://secret-hamlet-67983.herokuapp.com/')
 def main():
     return render_template('main.html')
 
